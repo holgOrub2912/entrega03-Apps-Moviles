731df18508eb0ff2bdd640fac58e2be83af3dbb1
Fernando González Rivero <fernando.gonzalezr@upb.edu.co>
2026-04-17 06:37:53 -0500

    Implement exito search feature

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,6 +88,11 @@ dependencies {
 
     // JSON serialization library, works with the Kotlin serialization plugin
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    // Retrofit with kotlin serialization
+    implementation("com.squareup.retrofit2:converter-kotlinx-serialization:2.11.0")
 }
 
 apollo {
